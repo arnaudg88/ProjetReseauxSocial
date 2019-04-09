e57240a4deb0e9a97aec8c84ab8c48e23ae39848
arnaud gerard <nono88100@gmail.com>
2019-04-09 09:08:14 +0200

    initialisation du projet maven

---
+++ src/main/java/fr/ul/miage/ProjetReseauxSocial/App.java
@@ -0,0 +1,13 @@
+package fr.ul.miage.ProjetReseauxSocial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
